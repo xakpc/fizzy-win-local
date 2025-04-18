247524da2960a9d1797134f34b3df2ff577f8c64
David Heinemeier Hansson <david@37signals.com>
2025-04-18 16:47:53 +0200

    Stlye

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -1,10 +1,12 @@
 module WorkflowsHelper
   def button_to_set_stage(card, stage)
     button_to \
-        tag.span(stage.name, class: "overflow-ellipsis"), card_stagings_path(card, stage_id: stage),
-        method: :post, class: [ "btn justify-start workflow-stage txt-uppercase workflow-stage", { "workflow-stage--current": stage == card.stage } ],
-        form_class: "flex align-center gap-half",
-        data: { turbo_frame: "_top" }
+      tag.span(stage.name, class: "overflow-ellipsis"),
+      card_stagings_path(card, stage_id: stage),
+      method: :post,
+      class: [ "btn justify-start workflow-stage txt-uppercase workflow-stage", { "workflow-stage--current": stage == card.stage } ],
+      form_class: "flex align-center gap-half",
+      data: { turbo_frame: "_top" }
   end
 
   def stage_color(stage)
